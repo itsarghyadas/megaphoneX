81599adaaaff9d70191aea4a280556920a510927
Arghya Das <arghyadasproject@gmail.com>
2023-10-26 12:51:10 +0530

    unnecessary ui component deleted

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,10 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import SiteNav from "@/components/navbar";
-import { Toaster } from "@/components/ui/toaster";
 
-const bricolageGrotesque = Bricolage_Grotesque({ subsets: ["latin"] });
+const bricolageGrotesque = Bricolage_Grotesque({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "megaphoneX",
@@ -23,7 +24,6 @@ export default function RootLayout({
         <body className={bricolageGrotesque.className}>
           <SiteNav />
           {children}
-          <Toaster />
         </body>
       </html>
     </ClerkProvider>
