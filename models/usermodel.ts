a75cd5338de29e4e2062ecc99fbcb8cefe09426d
Arghya Das <arghyadasproject@gmail.com>
2023-11-17 10:53:14 +0530

    megaphonex update to stripe pricing total login webhook user data to mongodb database

--- models/usermodel.ts
+++ models/usermodel.ts
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  user_id: {
-    type: String,
-    required: true,
-    unique: true,
+const UserSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    useremail: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    credits: {
+      type: Number,
+      default: 0,
+    },
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  useremail: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  credits: {
-    type: Number,
-    default: 0,
-  },
-});
+  { collection: "users" }
+);
 
 const User = mongoose.model("User", UserSchema);
 
