fc60d7ac0c13efca36ceded8e8187947716a661c
Arghya Das <arghyadasproject@gmail.com>
2023-11-17 12:15:25 +0530

    megaphonex update to stripe pricing total credits in mongodb database

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,24 @@ export default function Home() {
         <h1 className="font-black bg-gradient-to-br from-[#af8bee] via-rose-500 to-[#6903f6] text-transparent bg-clip-text text-center max-w-sm md:max-w-xl lg:max-w-4xl mx-auto text-4xl md:text-6xl lg:text-7xl">
           Free giveaway tool for Pre-Launch
         </h1>
-        <Link
-          href="/form"
-          className={`rounded-full ${buttonVariants({
-            variant: "default",
-          })} h-11 w-fit px-14 text-[18px] text-white bg-[radial-gradient(100%_100%_at_100%_0%,_#af8bee_0%,_#6903f6_100%)] transition-[box-shadow_0.15s_ease,_transform_0.15s_ease] shadow-[2px_2px_0px_2px_rgba(0,0,0,0.8)] hover:shadow-[2px_2px_0px_2px_rgba(0,0,0,0.8)] hover:scale-[.98] active:-translate-y-0.5 active:shadow-[inset_0px_3px_7px_#6903f6] hover:text-white`}
-        >
-          Start your giveaway
-        </Link>
+        <div className="flex flex-col md:flex-row gap-y-2 items-center gap-x-5">
+          <Link
+            href="/form"
+            className={`rounded-full ${buttonVariants({
+              variant: "default",
+            })} h-11 w-fit px-14 text-[18px] text-white bg-[radial-gradient(100%_100%_at_100%_0%,_#af8bee_0%,_#6903f6_100%)] transition-[box-shadow_0.15s_ease,_transform_0.15s_ease] shadow-[2px_2px_0px_2px_rgba(0,0,0,0.8)] hover:shadow-[2px_2px_0px_2px_rgba(0,0,0,0.8)] hover:scale-[.98] active:-translate-y-0.5 active:shadow-[inset_0px_3px_7px_#6903f6] hover:text-white duration-100 ease-linear`}
+          >
+            Start your giveaway
+          </Link>
+          <Link
+            href="#"
+            className={`rounded-full ${buttonVariants({
+              variant: "default",
+            })} h-11 w-fit px-14 text-[18px] text-white bg-[radial-gradient(100%_100%_at_100%_0%,_orange_0%,_red_100%)] transition-[box-shadow_0.15s_ease,_transform_0.15s_ease] shadow-[2px_2px_0px_2px_rgba(0,0,0,0.8)] hover:shadow-[2px_2px_0px_2px_rgba(0,0,0,0.8)] hover:scale-[.98] active:-translate-y-0.5 active:shadow-[inset_0px_3px_2px_red] hover:text-white duration-100 ease-linear`}
+          >
+            Check Pricing
+          </Link>
+        </div>
       </div>
     </main>
   );
